8eb6c8231b2b4d9e086bb2b2e062143596ef7772
AnastasiaKlets <klecz.anastasia@gmail.com>
2023-06-07 23:39:40 +0300

    fix style 4 part

--- src/components/emotionality/Emotionality.tsx
+++ src/components/emotionality/Emotionality.tsx
@@ -4,51 +4,126 @@ import './Emotionality.css'
 import okImg from './img/ok.svg'
 import happyImg from './img/happy.svg'
 import angryImg from'./img/angry.svg'
+import { cn } from "@bem-react/classname";
+
+function chekProcent(inf:any){
+    if(inf.items.procent < 5){
+        return 1
+    }
+    if(inf.items.procent < 10){
+        return 2
+    }
+    if(inf.items.procent<20){
+        return 3
+    }
+    if(inf.items.procent < 30){
+        return 4
+    }
+    if(inf.items.procent<40){
+        return 5
+    }
+    if(inf.items.procent < 50){
+        return 6
+    }
+    if(inf.items.procent<60){
+        return 7
+    }
+    if(inf.items.procent<70){
+        return 8
+    }
+    if(inf.items.procent < 80){
+        return 9
+    }
+    if(inf.items.procent<90){
+        return 10
+    }
+    if(inf.items.procent < 100){
+        return 11
+    }
+
+}
 
 function Emotionality() {
-    let infForEmotionality = {
-        subtitle1: "Процент ваших эмоций в выступлении.",
-        value1: 9,
-        value2: 25,
-        value3: 90
-    };
+
+    let inf= {
+        title:"Процент ваших эмоций в выступлении",
+        items:[
+            {
+                title:"Злость",
+                procent:10,
+                colorProcent: "#FF4E78"
+            },
+            {
+                title:"Без эмоций",
+                procent:25,
+                colorProcent: "#7C8EB5"
+            },
+            {
+                title:"Радость",
+                procent:25,
+                colorProcent: "#10CE7E"
+            }
+        ]
+    }
+    const cnProfilePreview = cn("imk");
+    // @ts-ignore
+    const listItems = inf.items.filter(( str => str.title == "Злость")).map((number) =>
+        <>
+            <div >
+                <ReactSVG className={} src={angryImg}/>
+            </div>
+            <p className="pText">
+                {number.title}
+            </p>
+            <p className="pPercent" style={{color:number.colorProcent}}>
+                {number.procent} %
+            </p>
+        </>
+    );
+    const listItemsok = inf.items.filter(( str => str.title == "Без эмоций")).map((number) =>
+        <>
+            <div >
+                <ReactSVG className={chekProcent(number.procent)} src={okImg}/>
+            </div>
+            <p className="pText">
+                {number.title}
+            </p>
+            <p className="pPercent" style={{color:number.colorProcent}}>
+                {number.procent} %
+            </p>
+        </>
+    );
+    const listItemshappy = inf.items.filter(( str => str.title == "Радость")).map((number) =>
+        <>
+            <div >
+                <ReactSVG className={chekProcent(number.procent)} src={happyImg}/>
+            </div>
+            <p className="pText">
+                {number.title}
+            </p>
+            <p className="pPercent" style={{color:number.colorProcent}}>
+                {number.procent} %
+            </p>
+        </>
+    );
+
+
     return (
         <>
             <div className="textEmotionality">
                 <p>
-                    {infForEmotionality.subtitle1}
+                    {inf.title}
                 </p>
             </div>
             <div className="imgAllEmotionality">
                 <div className="angryImg">
-                    <div >
-                        <ReactSVG className="imk1" src={angryImg}/>
-
-                    </div>
-                    <p className="pText">
-                        Злость
-                    </p>
-                    <p className="pPercent" style={{color: "#FF4E78"}}>
-                        {infForEmotionality.value1} %
-                    </p>
+                    {listItems}
                 </div>
                 <div className="okImg">
-                    <ReactSVG className="imk2" src={okImg}/>
-                    <p className="pText">
-                        Нейтрально
-                    </p>
-                    <p className="pPercent" style={{color: "#7C8EB5"}}>
-                        {infForEmotionality.value2} %
-                    </p>
+                    {listItemsok}
                 </div>
                 <div className="happyImg">
-                    <ReactSVG className="imk3" src={happyImg} />
-                    <p className="pText">
-                        Радость
-                    </p>
-                    <p className="pPercent" style={{color: "#10CE7E"}}>
-                        {infForEmotionality.value3} %
-                    </p>
+                    {listItemshappy}
                 </div>
             </div>
         </>
--- src/components/informative/Informative.tsx
+++ src/components/informative/Informative.tsx
@@ -2,23 +2,46 @@ import React from 'react';
 import './Informative.css'
 
 function Informative() {
-    let infForInformative = {
-        subtitle1: "Доля неречевых звуков и слов-паразитов превышает допустимый порог.",
-        subtitle2: "Не хватает фактов и деталей для подтерждения высказанных аргументов."
+    let inf= {
+        title:"Доля неречевых звуков и слов-паразитов превышает допустимый порог.",
+        items:[
+            {
+                title:"Слова-паразиты",
+                value:"10%",
+                color:"#410DAE",
+            },
+            {
+                title:"Неречевые звуки",
+                value:"25%",
+                color:"#FE5D74",
+            },
+            {
+                title:"Аргументы без подтверждения",
+                value:"25%",
+                color:"#FFB800",
+            },
+            {
+                title:"Другое",
+                value:"40%",
+                color:"#7C8EB5",
+            }
+        ]
     }
+
+    const listItems = inf.items.map((number) =>
+            <div className="content" style={{backgroundColor: number.color, width: number.value, }}>
+                {number.title}
+            </div>
+    );
 
     return (
         <>
             <div className="inf">
                 <div className="text">
-                    <p> {infForInformative.subtitle1}</p>
-                    <p>{infForInformative.subtitle2}</p>
+                    <p> {inf.title}</p>
                 </div>
-                <div className="flex-container">
-                    <div className="parasites">Слова-паразиты</div>
-                    <div className="Non-speech">Неречевые звуки</div>
-                    <div className="Arguments">Аргументы без подтверждения</div>
-                    <div className="Other">Другое в выступлении</div>
+                <div className="container">
+                    {listItems}
                 </div>
             </div>
         </>
